4940f60d9904cf0a8e9809d1ab6d64c446955fd5
Adias04 <115788945+Adias04@users.noreply.github.com>
2023-02-24 21:55:52 +0530

    Update University_prediction.py

--- University_prediction.py
+++ University_prediction.py
@@ -37,34 +37,40 @@ def get_university_ratings(gre_score, toefl_score):
     else:
         return None
 
-# main program loop
-while True:
-    speak("What is your GRE score?")
-    gre_score_text = listen()
-    if not gre_score_text.isdigit():
-        speak("Please provide a valid GRE score.")
-        continue
-    gre_score = int(gre_score_text)
-    speak(f"Your GRE score is {gre_score}.")
+# function that encloses all program functions
+def admission_service():
+    # main program loop
+    while True:
+        speak("What is your GRE score?")
+        gre_score_text = listen()
+        if not gre_score_text.isdigit():
+            speak("Please provide a valid GRE score.")
+            continue
+        gre_score = int(gre_score_text)
+        speak(f"Your GRE score is {gre_score}.")
 
-    speak("What is your TOEFL score?")
-    toefl_score_text = listen()
-    if not toefl_score_text.isdigit():
-        speak("Please provide a valid TOEFL score.")
-        continue
-    toefl_score = int(toefl_score_text)
-    speak(f"Your TOEFL score is {toefl_score}.")
+        speak("What is your TOEFL score?")
+        toefl_score_text = listen()
+        if not toefl_score_text.isdigit():
+            speak("Please provide a valid TOEFL score.")
+            continue
+        toefl_score = int(toefl_score_text)
+        speak(f"Your TOEFL score is {toefl_score}.")
 
-    university_ratings = get_university_ratings(gre_score, toefl_score)
-    if university_ratings is not None:
-        speak("You can apply to the following universities:")
-        for rating in university_ratings:
-            speak(f"University with a rating of {rating}")
-    else:
-        speak("Sorry, we could not find any universities that match your scores.")
-    
-    speak("Do you want to try again?")
-    choice = listen()
-    if "yes" not in choice.lower():
-        speak("Thank you for using our service.")
-        break
+        university_ratings = get_university_ratings(gre_score, toefl_score)
+        if university_ratings is not None:
+            speak("You can apply to the following universities:")
+            for rating in university_ratings:
+                speak(f"University with a rating of {rating}")
+        else:
+            speak("Sorry, we could not find any universities that match your scores.")
+
+        speak("Do you want to try again?")
+        choice = listen()
+        if "yes" not in choice.lower():
+            speak("Thank you for using our service.")
+            break
+
+# call the admission service function
+admission_service()
+
