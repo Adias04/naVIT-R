12f524b0ca00929373e918b48298a7409820efaa
Adias04 <115788945+Adias04@users.noreply.github.com>
2023-03-16 21:44:03 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,25 @@
 import speech_recognition as sr
 import pyttsx3
+import random
+from google.oauth2 import service_account
+from google.api_core.exceptions import InvalidArgument
+from google.cloud import dialogflow_v2
+import wikipedia
 
-# initialize the text-to-speech engine
 engine = pyttsx3.init()
 
-# function to convert text to speech
+
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-# function to convert speech to text
+
 def listen():
     r = sr.Recognizer()
-    r.energy_threshold = 1000 # adjust as necessary
+    r.energy_threshold = 1000 
     with sr.Microphone() as source:
         print("Speak now...")
-        audio = r.listen(source, phrase_time_limit=10) # adjust as necessary
+        audio = r.listen(source, phrase_time_limit=10) 
     try:
         text = r.recognize_google(audio)
         print(f"You said: {text}")
@@ -24,26 +28,60 @@ def listen():
         print("Sorry, could not recognize your voice.")
         return ""
 
-# main program loop
+def detect_intent(project_id, session_id, text):
+    credentials = service_account.Credentials.from_service_account_file("C:\\Users\\hp\\Downloads\\chatbot-ylkl-9959a6982fbe.json")
+    session_client = dialogflow_v2.SessionsClient(credentials=credentials)
+    session_path = session_client.session_path(project_id, session_id)
+
+    if not text:
+        return None
+
+    text_input = dialogflow_v2.types.TextInput(text=text, language_code='en-US')
+    query_input = dialogflow_v2.types.QueryInput(text=text_input)
+
+    try:
+        response = session_client.detect_intent(session=session_path, query_input=query_input)
+        return response.query_result.intent.display_name
+    except InvalidArgument:
+        raise
+
+
 while True:
-    speak("What would you like to search for? Say 'University Prediction' or 'Faculty Room Information'.")
     choice = listen().lower()
-    
-    if "university prediction" in choice:
+
+   
+    session_id = f"session{random.randint(1,1000)}"
+
+    intent_name = detect_intent('chatbot-ylkl', session_id, choice)
+
+    if intent_name == 'University_Prediction':
         import University_prediction
-        University_prediction.admission_service()
-    elif "faculty room information" in choice:
+        result = University_prediction.admission_service()
+        speak(f"The predicted university is {result}")
+        break
+
+    elif intent_name == 'faculty_details':
         import faculty_details_getter
         faculty_details_getter.run()
-    else:
-        speak("Sorry, I didn't understand what you said. Please try again.")
-        continue
+        break
+    elif intent_name == 'Min_credits':
+        import get_min_credit_branchwise
+        get_min_credit_branchwise.run2()
+        break
 
-    speak("Do you want to search for something else?")
-    choice = listen().lower()
-    if "no" in choice:
-        speak("Thank you for using our service.")
-        exit()
+    else:
+        try:
+            
+            page = wikipedia.page(choice)
+            summary = wikipedia.summary(choice, sentences=2)
+            speak(summary)
+        except wikipedia.exceptions.PageError:
+            speak("Sorry, I couldn't find any information on that topic. Please try again.")
+        except wikipedia.exceptions.DisambiguationError as e:
+            
+            options = ", ".join(e.options[:5])
+            speak(f"I found multiple pages that might match, including {options}. Could you please be more specific?")
+        break
 
 
     
